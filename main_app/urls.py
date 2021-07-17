8d73439cf4ddf0f26c58b9152891a55ddc578a8a
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-07-17 10:31:20 -0700

    Added in Login Functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('locations/', views.location_index, name='index'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
