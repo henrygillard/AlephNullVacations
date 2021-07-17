635340c21be86f843f763781797f9a8b2aa76699
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-07-17 10:46:12 -0700

    Add in Signout & NewAccount

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Location
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Define the home view
 def home(request):
@@ -11,3 +13,17 @@ def about(request):
 def location_index(request):
     locations = Location.objects.all()
     return render(request, 'locations/index.html', { 'locations': locations})
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
