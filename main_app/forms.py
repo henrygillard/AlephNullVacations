3b9c7cc6450b11b5cc8a52461f1adc12cf6ed29d
Henry Gillard <henry@MacBook-Pro.local>
2021-07-19 09:43:40 -0700

    Reviews

---
+++ main_app/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Review
+
+class ReviewForm(ModelForm):
+  class Meta:
+    model = Review
+    fields = ['rating', 'content']
--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
 
+RATINGS = (
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+
+)
 
 class Location(models.Model):
     name = models.CharField(max_length=60)
@@ -12,9 +20,24 @@ class Location(models.Model):
     longitude = models.IntegerField(null=True, blank=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self):
         return reverse('location_detail', kwargs={'location_id': self.id})
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'location_id': self.id})
+
+class Review(models.Model):
+    content = models.CharField(max_length=500)
+    rating = models.CharField(
+        "rating",
+        max_length=1,
+        choices=RATINGS,
+        default=RATINGS[0][0]
+    )
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.content}"
